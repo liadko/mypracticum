a08e16b28eb91f24dd183ffa42a3893612e4e453
Liad Koren <liad.koren@gong.io>
2025-11-01 16:19:25 +0200

    Stop tracking 'run server.bat'

--- backend/go-app/run server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d %~dp0
-
-REM ─── Run migrations ───────────────
-set "DATABASE_URL=postgresql://neondb_owner:npg_YML31gbEKrmH@ep-weathered-flower-a25hq2rj-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require&prefer_simple_protocol=true"
-migrate -path db/migrations -database "%DATABASE_URL%" up
-
