295766b9fd48962dc4aa17e6d1b987c87678ba22
Liad Koren <liad.koren@gong.io>
2025-11-15 18:43:29 +0200

    Cleanup the Java (second java project that i delete from practicum...)

--- backend/java-admin-portal/src/main/java/com/practicum/admin/dto/manual_entry/FailedManualEntry.java
+++
@@ -1,9 +0,0 @@
-package com.practicum.admin.dto.manual_entry;
-
-/**
- * Represents a single failed entry in the response.
- */
-public record FailedManualEntry(
-        NewManualEntryInput input,
-        String error
-) {}
